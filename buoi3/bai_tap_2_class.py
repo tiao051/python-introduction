b39fbb1c82ef403389cd3b21643f01dd07db0b82
tiao051 <nguyenminhtho0503@gmail.com>
2026-01-28 14:54:44 +0700

    feat: add StringHandler class for string input and output handling

---
+++ buoi3/bai_tap_2_class.py
@@ -0,0 +1,20 @@
+class StringHandler:
+    def __init__(self):
+        self._string = ""
+    
+    @property
+    def string(self):
+        return self._string
+    
+    def get(self):
+        self._string = input("Nhập một chuỗi: ")
+    
+    def put(self):
+        print(f"Chuỗi: {self.string}")
+
+handler = StringHandler()
+
+handler.get()
+handler.put()
+
+print("\nThoát chương trình.")
