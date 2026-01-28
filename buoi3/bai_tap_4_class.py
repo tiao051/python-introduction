b8720bd688c36da64cc0ee701f412eb16bc09662
tiao051 <nguyenminhtho0503@gmail.com>
2026-01-28 16:00:53 +0700

    feat: add Employee class for managing employee information and calculations

---
+++ buoi3/bai_tap_4_class.py
@@ -0,0 +1,58 @@
+class Employee:
+    def __init__(self):
+        self._name = ""
+        self._age = 0
+        self._address = ""
+        self._salary = 0.0
+        self._total_hours = 0
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def age(self):
+        return self._age
+    
+    @property
+    def address(self):
+        return self._address
+    
+    @property
+    def salary(self):
+        return self._salary
+    
+    @property
+    def total_hours(self):
+        return self._total_hours
+    
+    def inputInfo(self):
+        self._name = input("Nhập tên nhân viên: ")
+        self._age = int(input("Nhập tuổi: "))
+        self._address = input("Nhập địa chỉ: ")
+        self._salary = float(input("Nhập tiền lương: "))
+        self._total_hours = int(input("Nhập tổng số giờ làm: "))
+    
+    def printInfo(self):
+        print(f"Tên: {self.name}")
+        print(f"Tuổi: {self.age}")
+        print(f"Địa chỉ: {self.address}")
+        print(f"Tiền lương: {self.salary:.2f}")
+        print(f"Tổng số giờ làm: {self.total_hours}")
+        print(f"Tiền thưởng: {self.tinhThuong():.2f}")
+    
+    def tinhThuong(self):
+        if self.total_hours >= 200:
+            return self.salary * 0.2
+        elif self.total_hours >= 100:
+            return self.salary * 0.1
+        else:
+            return 0
+
+employee = Employee()
+
+employee.inputInfo()
+print()
+employee.printInfo()
+
+print("\nThoát chương trình.")
